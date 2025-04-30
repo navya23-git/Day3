0f04fa591ed24809dea58422fc5730d1650fddcc
navya23-git <navyasoma683@gmail.com>
2025-04-30 19:11:09 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator{
+public static int add(int a, int b){
+return a+b;
+}
+public static int substract(int a,int b){
+return a-b;
+}
+public static void main(String[] args){
+System.out.println(Calculator.add(10,5));
+System.out.println(Calculator.substract(10,5));
+}}
---
+++ StringUtility.java
@@ -0,0 +1,7 @@
+public class StringUtility{
+public static int getLength(String input){
+return input.length();
+}
+public static void main(String args[]){
+System.out.println(StringUtility.getLength("Hello"));
+}}
